19d6981366f305520462c9660198401748a5410d
Shivakumar Kadli <155358438+shivakumarkadli4272@users.noreply.github.com>
2024-10-13 18:11:45 +0530

    Create simple-hello.sh
    Its just the sample

---
+++ simple-hello.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+<<comment
+This is multiline comment
+comment
+
+<<Warning
+This is multiline comment
+Warning
+
+
+echo "  "
+echo "Shiva: Kithne aadmi the??"
+echo "  "
+echo "Abdul: Pata nahi Shiva bhaiii!!!!!"
+echo "  "
+echo "Kithne bhaje hai?"
+echo "  "
+date
+echo "  "
+echo "Kithne bhaje se machine on hai?"
+uptime
+echo "  "
