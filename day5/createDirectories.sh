f69f3b63d5084fa0322454adf54aaad3d9b8622f
Shivakumar Kadli <155358438+shivakumarkadli4272@users.noreply.github.com>
2024-10-20 01:02:44 +0530

    Update and rename createDirectories.sh to day5/createDirectories.sh

--- day5/createDirectories.sh
+++ day5/createDirectories.sh
@@ -5,7 +5,6 @@
 if [ $# -ne 3 ]; then
         echo "Usage: The script $0 is not executed with 3 arguments .ie day 1 90"
         exit 1;
-
 fi
 
 #Assign arguments to variables
@@ -20,5 +19,4 @@ for (( i=start_num; i<end_num; i++))
 do
         mkdir "${dir_name}${i}"
 done
-
 echo " Directories created from ${dir_name}${start_num} to ${dir_name}${end_num} "
