7ac950a2e4f13fdba19e24f21b5c1f9cf9dea351
Shivakumar Kadli <155358438+shivakumarkadli4272@users.noreply.github.com>
2024-10-20 01:01:23 +0530

    Create task3.sh

---
+++ day8/task3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Task 3 to use the variables to print output
+name=Shivakumar
+course=90DaysOfDevops
+
+echo "Happy to share,me $name have enrolled and learning $course, Thanks Shubham"
